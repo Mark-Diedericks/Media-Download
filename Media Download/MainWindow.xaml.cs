365536b01ed558e93d673f36abc1f271f6248d94
Mark-Diedericks <Mark-Diedericks@users.noreply.github.com>
2020-03-04 22:02:58 +1100

    Cleaned dependencies

--- Media Download/MainWindow.xaml.cs
+++ Media Download/MainWindow.xaml.cs
@@ -5,22 +5,13 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Json;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace Media_Download
